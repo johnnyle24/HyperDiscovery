80a444b5170d3e69b7e018c457cebd6588ea8970
Jesus <jesus.zarate.eng@gmail.com>
2018-03-01 18:33:45 -0700

    Fixed bugs for cade lab

--- Misc/splitAndTokenize.py
+++ Misc/splitAndTokenize.py
@@ -1,4 +1,5 @@
 import nltk
+import os
 
 def read_in_chunks(file_object, chunk_size=1024):
     """
@@ -24,13 +25,35 @@ def write(sentence, filename = 'testfile.txt'):
     with open(filename, 'w') as file:
         file.write(str(sentence))
 
+def corpusDirectoryExist(head, dirname):
+    fullPath = os.path.join(head, dirname)
+    if not os.path.isdir(fullPath):
+        os.mkdir(os.path.join(head, dirname))
+
+    return fullPath
+
 if __name__ == '__main__':
-    corpus = '../Data/2B_music_bioreviews_tokenized.txt'
+    # corpus = '../Data/2B_music_bioreviews_tokenized.txt'
     # corpus = '../Data/2A_med_pubmed_tokenized.txt'
     # corpus = '../Data/musc/2B_music_bioreviews_2.txt'
     # corpus = '../Data/2A_med_pubmed_tokenized.txt'#sys.argv[1]
 
-    f=open(corpus,'rU')
+    corpuses = ['../Data/2B_music_bioreviews_tokenized.txt', '../Data/2A_med_pubmed_tokenized.txt']
 
-    for chunk in read_in_chunks(f, 1000000):
-        write(pos(chunk))
+
+    for corpus in corpuses:
+
+        if os.path.isfile(corpus):
+            continue
+
+        head, tail = os.path.split(corpus)
+        f=open(corpus,'rU')
+
+        # filename = tail.replace('', '.txt')
+        filename, ext = os.path.splitext(tail)
+        head = corpusDirectoryExist(head, filename)
+
+
+        for i, chunk in enumerate(read_in_chunks(f, 1000000)):
+            newFilename = os.path.join(head, '{0}_{1}.txt'.format(filename, i))
+            write(pos(chunk), filename=newFilename)
